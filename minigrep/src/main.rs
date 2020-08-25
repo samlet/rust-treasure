a8e40e7c53508b538594093ea0bff1210c02945d
Samlet Wu <xiaofei.wu@gmail.com>
2020-08-26 01:06:38 +0800

    直接使用 env::args 返回的迭代器

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -10,8 +10,7 @@ use minigrep;
 // $ CASE_INSENSITIVE=1 cargo run to poem.txt
 // $ cargo run to poem.txt > output.txt
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::new(env::args()).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
