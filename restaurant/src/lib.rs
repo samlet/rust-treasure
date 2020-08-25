e261a32f794368316630bb77553e4c730fe95f4c
Samlet Wu <xiaofei.wu@gmail.com>
2020-08-25 20:05:27 +0800

    simple tests

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -8,3 +8,28 @@ pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+}
+
