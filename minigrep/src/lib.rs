7d3630ff588af5bbfd44f9a6d9399783caf9f1c0
Samlet Wu <xiaofei.wu@gmail.com>
2020-08-25 22:39:40 +0800

    创建一个我们期望的 search 函数的失败测试

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -8,6 +8,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -23,6 +24,29 @@ impl Config {
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
     }
 }
 
