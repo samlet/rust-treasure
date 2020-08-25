6db99cc3c2dea1c16d1de206bb0b24198cacef57
Samlet Wu <xiaofei.wu@gmail.com>
2020-08-25 22:15:09 +0800

    add run function with result

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -14,16 +14,20 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
-}
 
+    Ok(())
+}
 struct Config {
     query: String,
     filename: String,
