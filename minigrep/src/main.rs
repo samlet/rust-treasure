8abd2a7d4e46564d04d3619565eaef6aba8d67a9
Samlet Wu <xiaofei.wu@gmail.com>
2020-08-25 22:00:46 +0800

    args as tuple

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,8 +5,9 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    // let query = &args[1];
+    // let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
@@ -15,5 +16,12 @@ fn main() {
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
 
