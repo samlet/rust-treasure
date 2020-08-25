9cf0fb9bdfe35ee16bc9caf257d5ca645d76ba78
Samlet Wu <xiaofei.wu@gmail.com>
2020-08-25 20:16:38 +0800

    unit-test: panic message

--- restaurant/src/guess.rs
+++ restaurant/src/guess.rs
@@ -4,8 +4,12 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}.", value);
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {}.",
+                   value);
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}.",
+                   value);
         }
 
         Guess {
@@ -20,7 +24,7 @@ mod tests {
 
     // 使用 should_panic 检查 panic
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
